cea7115e921a90cec10a7f2843e5df7e8cc221ed
Phillip Strom <phillip.strom@gmail.com>
2020-09-01 15:05:49 -0600

    Add assertions to make tests more robust

--- spec/requests/api/v1/merchants/request_spec.rb
+++ spec/requests/api/v1/merchants/request_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'Merchants API' do
         name: Faker::Company.name
       )
     end
+    @merchant = Merchant.first
   end
 
   it 'sends a list of merchants' do
@@ -15,17 +16,38 @@ RSpec.describe 'Merchants API' do
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body)['data']
-
+    expect(response.content_type).to eq("application/json")
+    expect(merchants.first["id"]).to eq(@merchant.id.to_s)
     expect(merchants.count).to eq(10)
+    expect(merchants.first["type"]).to eq("merchant")
+    expect(merchants.first["attributes"]["name"]).to eq(@merchant.name)
   end
   it "sends a single merchant" do
     merchant = Merchant.last
 
     get "/api/v1/merchants/#{merchant.id}"
 
-    returned_item = JSON.parse(response.body)['data']
+    returned_merchant = JSON.parse(response.body)['data']
 
     expect(response).to be_successful
-    expect(returned_item["id"]).to eq(merchant.id.to_s)
+    expect(response.content_type).to eq("application/json")
+    expect(returned_merchant["id"]).to eq(merchant.id.to_s)
+    expect(returned_merchant["id"]).to eq(merchant.id.to_s)
+    expect(returned_merchant["attributes"]["name"]).to eq(merchant.name)
+  end
+  it "can create a merchant" do
+    merchant_params = { name: "Business!" }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post '/api/v1/merchants', headers: headers, params: JSON.generate(merchant_params)
+
+    merchant = Merchant.last
+    returned_merchant = JSON.parse(response.body)['data']
+
+    expect(response).to be_successful
+    expect(response.content_type).to eq("application/json")
+    expect(returned_merchant["type"]).to eq("merchant")
+    expect(returned_merchant["id"]).to eq(merchant.id.to_s)
+    expect(returned_merchant["attributes"]["name"]).to eq(merchant_params[:name])
   end
 end
