710833ae348b9f9936e89de4c9701be3fdb5208c
Phillip Strom <phillip.strom@gmail.com>
2020-09-02 16:29:08 -0600

    Create self.search method

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,12 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
+
+  def self.search(search_params)
+    items = Array.new
+    search_params.each do |attribute, search|
+      items << where("cast(#{attribute} as text) ILIKE ?", "%#{sanitize_sql_like(search)}%")
+    end
+    items.flatten
+  end
 end
