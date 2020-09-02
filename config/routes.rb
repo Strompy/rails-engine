a280ec0d412313ea392fe16d65f88d54c839a59b
Phillip Strom <phillip.strom@gmail.com>
2020-09-02 14:26:41 -0600

    create search merchant show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
   namespace :api do
       namespace :v1 do
+        namespace :items do
+          get '/:id/merchant', to: 'merchant#show'
+        end
+
+        namespace :merchants do
+          get '/find', to: 'search#show'
+        end
+
         resources :merchants, only: [:index, :show, :create, :update, :destroy] do
           resources :items, only: [:index], module: :merchants
         end
+
         resources :items, only: [:index, :show, :create, :update, :destroy] do
-          # resources :merchant, only: [:show], module: :items
-           # get '/merchant', to: 'merchant#show'
-        end
-        namespace :items do
-          get '/:id/merchant', to: 'merchant#show'
         end
       end
     end
