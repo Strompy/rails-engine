92b66141a00c67f0369b64f6191481aaf5274df1
Phillip Strom <phillip.strom@gmail.com>
2020-09-02 16:28:59 -0600

    Create show action

---
+++ app/controllers/api/v1/items/search_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::Items::SearchController < ApplicationController
+  def index
+    items = Item.search(search_params)
+    render json: ItemSerializer.new(items)
+  end
+
+  def show
+    require "pry"; binding.pry
+    merchant = Item.search(search_params).first
+    render json: ItemSerializer.new(merchant)
+  end
+
+  def search_params
+    params.permit(:id, :name, :description, :unit_price, :created_at, :updated_at)
+  end
+
+end
