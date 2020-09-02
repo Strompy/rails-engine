d233aa271386c7408b1e5d0c62680478a48d16bf
Phillip Strom <phillip.strom@gmail.com>
2020-09-02 12:32:34 -0600

    Create namespaced merchant show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   namespace :api do
       namespace :v1 do
         resources :merchants, only: [:index, :show, :create, :update, :destroy] do
-          # get '/items', to: 'merchant_items#index'
           resources :items, only: [:index], module: :merchants
         end
-        resources :items, only: [:index, :show, :create, :update, :destroy]
+        resources :items, only: [:index, :show, :create, :update, :destroy] do
+          # resources :merchant, only: [:show], module: :items
+           # get '/merchant', to: 'merchant#show'
+        end
+        namespace :items do
+          get '/:id/merchant', to: 'merchant#show'
+        end
       end
     end
 end
