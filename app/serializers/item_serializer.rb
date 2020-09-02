41060c839cc7a49a12a34e8855a3de54fc421d1b
Phillip Strom <phillip.strom@gmail.com>
2020-09-01 18:54:49 -0600

    Modify unit_price to match harness

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,6 +1,8 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :unit_price, :description, :merchant_id
-
+  attributes :name, :description, :merchant_id
+  attribute :unit_price do |object|
+    object.unit_price.to_f
+  end
   # belongs_to :merchant
 end
