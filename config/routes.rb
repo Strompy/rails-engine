e61c5956f7047450710f72cf418e7c1f72b71521
Phillip Strom <phillip.strom@gmail.com>
2020-09-02 12:11:05 -0600

    Create merchant items route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
       namespace :v1 do
-        resources :merchants, only: [:index, :show, :create, :update, :destroy]
+        resources :merchants, only: [:index, :show, :create, :update, :destroy] do
+          # get '/items', to: 'merchant_items#index'
+          resources :items, only: [:index], module: :merchants
+        end
         resources :items, only: [:index, :show, :create, :update, :destroy]
       end
     end
