b466ebe92d8b6b5c4fd53875d979a85a57254757
Phillip Strom <phillip.strom@gmail.com>
2020-09-02 12:10:58 -0600

    Create merchant items test

--- spec/requests/api/v1/merchants/merchant_items_request_spec.rb
+++ spec/requests/api/v1/merchants/merchant_items_request_spec.rb
@@ -25,5 +25,22 @@ RSpec.describe "Merchant Items Endpoint" do
     @item1 = Item.first
     @item2 = Item.last
   end
+  it "sends all items that belong to the Merchant" do
+     get "/api/v1/merchants/#{@merchant1.id}/items"
 
+     parsed = JSON.parse(response.body, symbolize_names: true)
+     items = parsed[:data]
+
+     expect(response).to be_successful
+     expect(response.content_type).to eq("application/json")
+     expect(items.count).to eq(5)
+     expect(items.first[:type]).to eq("item")
+     expect(items.first[:id]).to eq(@item1.id.to_s)
+     expect(items.first[:attributes][:name]).to eq(@item1.name)
+     expect(items.first[:attributes][:unit_price].to_f).to eq(@item1.unit_price.to_f)
+     expect(items.first[:attributes][:description]).to eq(@item1.description)
+     expect(items.first[:attributes][:description]).to eq(@item1.description)
+     expect(items.first[:attributes][:merchant_id]).to eq(@merchant1.id)
+     expect(items.none? { |item| item[:attributes][:merchant_id] == @merchant2.id}).to be true
+  end
 end
