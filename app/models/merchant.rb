80e6f0b70cd7a94ce12d7648c1fa625182b08f4c
Phillip Strom <phillip.strom@gmail.com>
2020-09-02 14:27:05 -0600

    Create model self.search method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,13 @@ class Merchant < ApplicationRecord
 
   has_many :items
   has_many :invoices
+
+  def self.search(search_params)
+    merchants = Array.new
+    search_params.each do |column, search|
+      merchants << where("cast(#{column} as text) ILIKE ?", "%#{sanitize_sql_like(search)}%")
+      # where("cast(id as text) ILIKE ?", "%2420%")
+    end
+    merchants.flatten
+  end
 end
