ed26579b17c219b1e38646958642906fb0b19fcb
Phillip Strom <phillip.strom@gmail.com>
2020-09-03 08:03:16 -0600

    add revenue route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
       namespace :v1 do
+
         namespace :items do
           get '/:id/merchant', to: 'merchant#show'
           get '/find', to: 'search#show'
@@ -13,6 +14,8 @@ Rails.application.routes.draw do
           get '/most_revenue', to: 'most_revenue#index'
           get '/most_items', to: 'most_items#index'
         end
+
+        get '/revenue', to: 'revenue#show'
 
         resources :merchants, only: [:index, :show, :create, :update, :destroy] do
           resources :items, only: [:index], module: :merchants
