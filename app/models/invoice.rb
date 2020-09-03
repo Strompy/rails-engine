a0e292935e221053f5a7e72d105217526718a530
Phillip Strom <phillip.strom@gmail.com>
2020-09-03 08:03:46 -0600

    Create revenue between dates method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,4 +6,13 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
   belongs_to :customer
   belongs_to :merchant
+
+  def self.revenue_between_dates(date_params)
+    start_date = date_params[:start]
+    end_date = date_params[:end]
+    Invoice.joins(:invoice_items, :transactions).
+    where("transactions.result='success'").
+    where(updated_at: Date.parse(start_date).beginning_of_day..Date.parse(end_date).end_of_day).
+    sum("invoice_items.unit_price * invoice_items.quantity")
+  end
 end
