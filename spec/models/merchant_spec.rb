c9311a1ae674b75841462e2e1ca83a09b8d1ef89
Phillip Strom <phillip.strom@gmail.com>
2020-09-02 18:20:14 -0600

    Create search method test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :items }
     it { should have_many :invoices }
   end
+  describe 'class methods' do
+    before :each do
+      Item.destroy_all
+      Merchant.destroy_all
+    end
+    it "can search with search_params" do
+      merchant = Merchant.create!(name: "Cool stuff")
+
+      search_params = { name: "Cool"}
+      expect(Merchant.search(search_params)).to eq([merchant])
+    end
+  end
 end
